6f755c22dc349952e2b6bf4c833023e6ed9aaf5f
nmcdonough <mcnoah94@gmail.com>
2018-08-27 11:55:46 -0400

    Updated Login/registration component

--- src/public/src/app/login/login.component.js
+++ src/public/src/app/login/login.component.js
@@ -1,25 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
-
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
-})
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-    var modal = document.getElementById('id01');
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
-	$("main").addClass("pre-enter").removeClass("with-hover");
+$("main").addClass("pre-enter").removeClass("with-hover");
 setTimeout(function(){
 	$("main").addClass("on-enter");
 }, 500);
@@ -49,5 +28,3 @@ $("h1 a").click(function(){
 		}, 50);
 	}
 });
-  }
-}
